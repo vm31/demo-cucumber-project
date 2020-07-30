96c113b4397be24cef8eb0eaf292baeac1a35714
middela <middela@nl1lxl-111480.ttg.global>
2020-07-30 16:29:59 +0200

    test

--- src/main/java/pages/BestsellersByQuantityPage.java
+++ src/main/java/pages/BestsellersByQuantityPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -32,37 +33,48 @@ public class BestsellersByQuantityPage {
     @FindBy(xpath = "//*[@id=\"bestsellers-byquantity-grid\"]//a[@href=\"/Admin/Product/Edit/34\"]")
     WebElement viewBtn4;
 
-    @FindBy(xpath = "//h1[@class='pull-left']")
-    WebElement editDisplayMsg;
+//    @FindBy(xpath = "//*[@id=\"product-form\"][@action=\"/Admin/Product/Edit/17\"]//h1")
+//    WebElement editDisplayMsg;
 
-    public String getProdutName1(){
-        return  productText1.getText();
-    }
-    public String getProdutName12(){
-        return  productText2.getText();
-    }
-    public String getProdutName3(){
-        return  productText3.getText();
-    }
-    public String getProdutName4(){
-        return  productText4.getText();
-    }
-    public void clickBViewBtn1(){
-        viewBtn1.click();
-    }
-    public void clickBViewBtn2(){
-        viewBtn2.click();
-    }
-    public void clickBViewBtn3(){
-        viewBtn3.click();
-    }
-    public void clickBViewBtn4(){
-        viewBtn4.click();
+    public String getProdutName1(int rowNumber){
+        WebElement productNameElem = ldriver.findElement(By.xpath("//*[@id=\"bestsellers-byquantity-grid\"]//a[@href=\"/Admin/Product/Edit/"+rowNumber+"]//../../td[1]"));
+        String x = productNameElem.getText();
+        System.out.println(x);
+        return  x;
     }
 
-    public String getEditPageMsg(){
-        return editDisplayMsg.getText();
+    public String getEditProductHeader(int rowNumber){
+        WebElement editHeader= ldriver.findElement(By.xpath("//*[@id=\"product-form\"][@action=\"/Admin/Product/Edit/"+rowNumber+"]//h1"));
+        String headerText = editHeader.getText();
+        return headerText;
     }
+
+
+//    public String getProdutName12(){
+//        return  productText2.getText();
+//    }
+//    public String getProdutName3(){
+//        return  productText3.getText();
+//    }
+//    public String getProdutName4(){
+//        return  productText4.getText();
+//    }
+//    public void clickBViewBtn1(){
+//        viewBtn1.click();
+//    }
+//    public void clickBViewBtn2(){
+//        viewBtn2.click();
+//    }
+//    public void clickBViewBtn3(){
+//        viewBtn3.click();
+//    }
+//    public void clickBViewBtn4(){
+//        viewBtn4.click();
+//    }
+//
+//    public String getEditPageMsg(){
+//        return editDisplayMsg.getText();
+//    }
 
 
 
--- src/test/java/stepDefinition/switchStep.java
+++ src/test/java/stepDefinition/switchStep.java
@@ -2,6 +2,7 @@ package stepDefinition;
 
 import baseTest.BaseTest;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import javafx.scene.effect.Light;
 import org.testng.Assert;
@@ -16,33 +17,43 @@ public class switchStep extends BaseTest {
 
     @And("I select box:{string} and verify page title is:{string},{string},{string}{string}")
     public void iSelectBoxAndVerifyPageTitleIs(String box, String title1, String title2, String title3, String title4) {
-            switch (box){
-                case "orders":
-                    switchPage.clickOrdersBox();
-                    Assert.assertEquals(title1, driver.getTitle());
-                    break;
+        switch (box) {
+            case "orders":
+                switchPage.clickOrdersBox();
+                Assert.assertEquals(title1, driver.getTitle());
+                break;
 
-                case "Pending return requests":
-                    switchPage.clickPendingBox();
-                    Assert.assertEquals(title2, driver.getTitle());
-                    break;
-                case "Registered customers":
-                    switchPage.clickRegisterBox();
-                    Assert.assertEquals(title3, driver.getTitle());
-                    break;
-                case "Low stock products":
-                    switchPage.clickLowStockBox();
-                    Assert.assertEquals(title4, driver.getTitle());
-                    break;
-                default:
-                    System.out.println("please specify the box");
-                    break;
+            case "Pending return requests":
+                switchPage.clickPendingBox();
+                Assert.assertEquals(title2, driver.getTitle());
+                break;
+            case "Registered customers":
+                switchPage.clickRegisterBox();
+                Assert.assertEquals(title3, driver.getTitle());
+                break;
+            case "Low stock products":
+                switchPage.clickLowStockBox();
+                Assert.assertEquals(title4, driver.getTitle());
+                break;
+            default:
+                System.out.println("please specify the box");
+                break;
 
-
-            }
 
         }
 
+    }
+
+    @Given("i test switch")
+    public void iTestSwitch(int rowNumber) {
+        String pdtName = bestsellersByQuantityPage.getProdutName1(rowNumber);
+        String headerName = bestsellersByQuantityPage.getEditProductHeader(rowNumber);
+        //case:1:16
+        // case:2:17
+        //case:3:18
+        //case:4:19
+        Assert.assertTrue(headerName.contains(pdtName));
+    }
 
 
 }
